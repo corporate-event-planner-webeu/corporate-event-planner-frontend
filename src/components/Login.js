605e7a4459affd063f735984099b04f1b34468ac
Anna W <anski.anna@gmail.com>
2019-06-24 13:22:46 +0100

    made some changes to the login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,13 @@ export default class Login extends React.Component {
     render(){
         return(
             <div>
+                <LoginWrapperStyled> 
+                    <h3>Log In Here</h3> 
+                    <input type="text" name="Email" placeholder="Email" required />
+                    <input type="passowrd" name="password" placeholder="Password" required />
+                    <ButtonStyled>Log in</ButtonStyled>
+                </LoginWrapperStyled>
+
                 <SignUpStyled>
                     <h3>Sign Up Here</h3>
                     <form>
@@ -13,25 +20,46 @@ export default class Login extends React.Component {
                         <input type="text" name="name" placeholder="Last name" />
                         <input type="text" name="email" placeholder="Email" />
                         <input type="text" name="name" placeholder="Password" />
-                        <button>Sign Up Now</button>
+                        <ButtonStyled>Sign Up Now</ButtonStyled>
                     </form>
                 </SignUpStyled>
-
-                <LoginWrapperStyled> 
-                    <h3>Log In Here</h3> 
-                    <input type="text" name="Email" placeholder="Email" required />
-                    <input type="passowrd" name="password" placeholder="Password" required />
-                    <div><button>Log in </button></div>
-                </LoginWrapperStyled>
             </div>
         )
     }
 }
-const SignUpStyled = styled.form`
+
+
+const LoginWrapperStyled = styled.form`
     border: 1px solid white;
+    border-radius: 2px;
     padding: 50px;
     background: #efefef;
     color: black;
+    margin: 20px;
+
+    input {
+        border: 1px solid rgba(255, 255, 255, 0.6);
+        border-radius: 2px;
+        padding-left: 10px;
+        margin:10px;
+        width: 220px;
+        height: 30px;
+    }
+    /* button {
+        width: 220px;
+        font-size: 20px;
+        background: white;
+    } */
+
+`; 
+
+const SignUpStyled = styled.form`
+    border: 1px solid white;
+    border-radius: 2px;
+    padding: 50px;
+    background: #efefef;
+    color: black;
+    margin: 20px;
     margin-bottom: 20px;
 
     input {
@@ -42,31 +70,15 @@ const SignUpStyled = styled.form`
         width: 220px;
         height: 30px;
     }
-    button {
+    /* button {
         width: 220px;
         font-size: 20px;
         background: white;
-    }
+    } */
 `;
 
-const LoginWrapperStyled = styled.form`
-    border: 1px solid white;
-    padding: 50px;
-    background: #efefef;
-    color: black;
-
-    input {
-        border: 1px solid rgba(255, 255, 255, 0.6);
-        border-radius: 2px;
-        padding-left: 10px;
-        margin:10px;
-        width: 220px;
-        height: 30px;
-    }
-    button {
+const ButtonStyled = styled.button`
         width: 220px;
         font-size: 20px;
         background: white;
-    }
-
-`; 
+`;
