e8242f5e0a47c7ed9767dd1ecf2dba19774b58fa
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-06-24 21:38:29 +0100

    Fix: Make change to the input field for adding a new event

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -5,10 +5,12 @@ export default class AddEvent extends Component {
         return (
             <div>
                 <p>Add new event</p>
-                <input type="text" placeholder="Event Name" />
+                <input type="text" placeholder="Event Title" />
+                <input type="text" placeholder="Event Description" />
                 <input type="text" placeholder="Budget" />
-                <input type="text" placeholder="Owner/client" />
+                <input type="text" placeholder="Number of Attendees" />
                 <input type="text" placeholder="Event Date" />
+                <input type="text" placeholder="Event Time" />
                 <button>Create Event</button>
             </div>
         )
