a48eacaa65097b888e54531ec582742800a92c3a
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-06-24 14:40:48 +0100

    Feat: Create and use middleware for setting token returned  during a successful login or signin to local storage

---
+++ src/middlewares/auth.js
@@ -0,0 +1,9 @@
+import { SUCCESSFUL_LOGIN, SUCCESSFUL_SIGNIN } from '../actions/actionTypes';
+import {set} from '../utils/localStorage';
+
+export const setToken = store => next => action => {
+    if(action.type === SUCCESSFUL_LOGIN || action.type === SUCCESSFUL_SIGNIN) {
+        set('userToken', action.payload);
+    }
+    next(action);
+}
