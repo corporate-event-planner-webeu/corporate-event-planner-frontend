ec8ab86c2cc9eec47da82e7cd8bc559cd865eb0f
Anna W <anski.anna@gmail.com>
2019-06-24 15:14:03 +0100

    error corrected

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import * as types from './actionTypes';
-import { setToken } from '../middlewares/auth';
-
 
 
 export const login = (email, password) => (dispatch) => {
@@ -9,8 +7,7 @@ export const login = (email, password) => (dispatch) => {
 
     axios.post('https://corporate-event-planner-webeu.herokuapp.com/api/auth/login', credentials)
         .then(res => {
-            dispatch(setToken())
-            // dispatch({ type: types.SUCCESSFUL_LOGIN, payload: res.token, message: 'login successful' }) 
+            dispatch({ type: types.SUCCESSFUL_LOGIN, payload: res.token, message: 'login successful' }) 
         })
         .catch(err => {
             console.log("Auth failed")
@@ -22,7 +19,7 @@ export const signup = (first_name, last_name, email, password, company, role) =>
 
     axios.post('https://corporate-event-planner-webeu.herokuapp.com/api/auth/register', credentials)
         .then(res => {
-            dispatch(setToken())
+            dispatch({ type: types.SUCCESSFUL_SIGNIN, payload: res.token, message: 'Registration successful' }) 
         })
         .catch(err => {
             console.log("Signup failed")
