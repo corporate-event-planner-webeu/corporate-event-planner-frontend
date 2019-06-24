0aaa7f85f8d8faca55c4375abe8d956a31e57c3f
Anna W <anski.anna@gmail.com>
2019-06-24 15:01:34 +0100

    still working on the login an dsignup action creators

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as types from './actionTypes';
+import { setToken } from '../middlewares/auth';
 
 
 
@@ -8,7 +9,8 @@ export const login = (email, password) => (dispatch) => {
 
     axios.post('https://corporate-event-planner-webeu.herokuapp.com/api/auth/login', credentials)
         .then(res => {
-            dispatch({ type: types.SUCCESSFUL_LOGIN, payload: res.token, message: 'login successful' }) 
+            dispatch(setToken())
+            // dispatch({ type: types.SUCCESSFUL_LOGIN, payload: res.token, message: 'login successful' }) 
         })
         .catch(err => {
             console.log("Auth failed")
@@ -20,6 +22,9 @@ export const signup = (first_name, last_name, email, password, company, role) =>
 
     axios.post('https://corporate-event-planner-webeu.herokuapp.com/api/auth/register', credentials)
         .then(res => {
-            dispatch({ type: types.SUCCESSFUL_SIGNIN, payload: res.token, message: 'sign up successful' })
+            dispatch(setToken())
+        })
+        .catch(err => {
+            console.log("Signup failed")
         })
 }
