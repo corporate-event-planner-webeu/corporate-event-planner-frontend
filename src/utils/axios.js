7cb67d0ddd352d111ac553b33f5d96f1e4defd16
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-06-25 16:07:52 +0100

    Feat: add return keyword to the axios create method

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default function() {
     const token = localStorage.getItem('userToken');
 
-    axios.create({
+    return axios.create({
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `${token}`
