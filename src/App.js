f4ca56f5c9f985b28ae2290a42b0da2bace1c42f
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-06-25 12:57:17 +0100

    Fix: Refactor code to remove redundant styles

--- src/App.js
+++ src/App.js
@@ -26,13 +26,7 @@ function App() {
 export default App;
 
 const AppWrapper = styled.div`
-  text-align: center;
-  background-color: #282c34;
-  min-height: 100vh;
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
 `;
