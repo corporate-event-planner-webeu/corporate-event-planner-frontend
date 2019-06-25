d443a1e13b2bb36cdf8cd57f6ae9f4de3d577589
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-06-25 12:58:37 +0100

    Feat: change background color

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import AddEvent from '../components/AddEvent';
-import Events from '../components/Events';
+import Event from '../components/Event';
+import styled from 'styled-components';
+
+const MainDiv = styled.div`
+  justify-content: space-between;
+  padding: 1rem;
+  border: 2px solid white;
+  min-height: 100vh;
+  background-color: rgb(233, 236, 240);
+  color: rgb(138, 146, 152);
+  text-align: left;
+
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 export default class Dashboard extends Component {
     render() {
         return (
-            <div>
+            <MainDiv>
                 <AddEvent />
-                <Events />
-            </div>
+                <Event />
+            </MainDiv>
         )
     }
 }
