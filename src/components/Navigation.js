8b0532d13cd7500bdce05ff3d6818c8801a3567b
Anna W <anski.anna@gmail.com>
2019-06-25 16:38:04 +0100

    added another navigation if user is logged in

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export default function Navigation() {
+export function Navigation() {
     return(
         <HeaderStyled>
             <h2>Event Planner</h2>
@@ -18,16 +18,29 @@ export default function Navigation() {
         </HeaderStyled>
     )
 }
+
+export function NavigationLoggedin(){
+    return(
+        <HeaderStyled>
+            <h2>Event Planner</h2>
+               <MenuStyled>
+                <ul>
+                    <Link className="link" to='/'>Home</Link>
+                    <Link className="link" to='/'>About</Link>
+                    <Link className="link " to='/'>Log Out</Link>
+                 </ul>
+                </MenuStyled>
+        </HeaderStyled>
+    )
+}
+
 const HeaderStyled = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     background-color: rgb(19, 28, 36);
     color: white;
-    /* margin:0px; */
     height: 60px;
-    /* padding-left: 5px;
-    padding-right:5px; */
     h2{
         margin: 15px 0;
         padding-left: 20px;
@@ -45,17 +58,16 @@ const MenuStyled = styled.div`
         color: white;
         text-align: center;
         padding-right: 40px;
-        /* padding-bottom: 20px; */
         padding-left: 40px;
-        /* margin-top: 40px; */
         font-size: 2rem;
     }
      .login {
-        /* border: 1px solid black;  */
-        /* border-radius: 10px;  */
         width: 20%;
         padding: 5px 0px 5px 0px;
         padding-bottom:0;
+    }
+    .link:hover{
+        color: yellow;
     }
 `;
 
