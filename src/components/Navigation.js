d09a63c1572d8afdabee07fe2f96ba85df54ef7a
Anna W <anski.anna@gmail.com>
2019-06-25 15:01:50 +0100

    added some more styling to the navigation, and another nav item

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Navigation() {
     return(
         <HeaderStyled>
             <h2>Event Planner</h2>
-            {/* <img class="menu-button" src="https://cdn3.iconfinder.com/data/icons/mobile-friendly-ui/100/menu-512.png" ></img> */}
                <MenuStyled>
-                {/* <ul> */}
+                <ul>
                     <Link className="link" to='/'>Home</Link>
                     <Link className="link" to='/'>About</Link>
-                    <Link className="link" to='./login'>Log In</Link> 
-                {/* </ul> */}
+                    <Link className="link " to='./login'>Log In</Link>
+                    <Link className="link " to='./signup'>Sign up</Link>
+
+                </ul>
                 </MenuStyled>
         </HeaderStyled>
     )
@@ -21,32 +22,38 @@ const HeaderStyled = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    background-color: #efefef;
+    vertical-align: center;
+    background-color: rgb(233,236,240);
     color: black;
-    margin:0px;
-    padding-left: 5px;
-    padding-right:5px;
-
-    
+    /* margin:0px; */
+    /* height: 100%; */
+    /* padding-left: 5px;
+    padding-right:5px; */
+        h2{
+            padding-left: 20px;
+        }
 `;
 
 const MenuStyled = styled.div`
-    /* ul{
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-    } */
+    ul{
+        margin-top: 35px;
+    }
     .link{
         align-content: center;
         text-decoration: none;
         color: black;
         text-align: center;
-        /* border: 1px solid black; */
-        border-radius: 10px;
-        padding: 10px;
-        margin-left: 50px;
-        margin-top: 20px;
-        width: 80%;
+        padding-right: 40px;
+        /* padding-bottom: 20px; */
+        padding-left: 40px;
+        margin-top: 40px;
+    }
+     .login {
+        border: 1px solid black; 
+        border-radius: 10px; 
+        width: 20%;
+        padding: 5px 0px 5px 0px;
+        padding-bottom:0;
     }
 `;
 
