dbc1405bb5a153c4c48b9b1f495b2a25218ce803
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-06-26 22:45:28 +0100

    Feat: Create action creator for uploading image

--- src/store/actions/event.js
+++ src/store/actions/event.js
@@ -1,4 +1,5 @@
 import axiosWithAuth from "../../utils/axios";
+import axios from "axios";
 
 import {
   FETCHING_EVENT,
@@ -6,14 +7,46 @@ import {
   DELETING_EVENT,
   COMPLETING_EVENT,
   SUCCESS_EVENT,
-  ERROR_EVENT
+  ERROR_EVENT,
+  ERROR_IMAGE,
+  SUCCESS_IMAGE,
+  UPLOADING_IMAGE
 } from "./actionTypes";
+
+export const uploadImage = file => dispatch => {
+  dispatch({type: UPLOADING_IMAGE})
+  const CLOUDINARY_URL =
+    "https://api.cloudinary.com/v1_1/ogwurujohnson/image/upload";
+  const CLOUDINARY_UPLOAD_PRESET = "zjjd4c1v";
+
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+
+  return axios
+    .post(CLOUDINARY_URL, formData)
+    .then(res => {
+      if (res.data.secure_url !== "") {
+        const uploadedFileUrl = res.data.secure_url;
+        dispatch({type: SUCCESS_IMAGE, payload: uploadedFileUrl, message: 'image uploaded'})
+      } else {
+        dispatch({type: ERROR_IMAGE, message: 'no image selected'})
+      }
+    })
+    .catch(err => dispatch({type: ERROR_IMAGE, message: 'Error: Try again shortly'}));
+};
 
 export const getAllEvents = url => dispatch => {
   dispatch({ type: FETCHING_EVENT });
   return axiosWithAuth()
     .get(url)
-    .then(res => dispatch({ type: SUCCESS_EVENT, payload: res.data, message: "Event fetched" }))
+    .then(res =>
+      dispatch({
+        type: SUCCESS_EVENT,
+        payload: res.data,
+        message: "Event fetched"
+      })
+    )
     .catch(err => dispatch({ type: ERROR_EVENT }));
 };
 
@@ -21,7 +54,13 @@ export const createEvent = (url, data) => dispatch => {
   dispatch({ type: ADDING_EVENT });
   return axiosWithAuth()
     .post(url, data)
-    .then(res => dispatch({ type: SUCCESS_EVENT, payload: res.data, message: "Event Created" }))
+    .then(res =>
+      dispatch({
+        type: SUCCESS_EVENT,
+        payload: res.data,
+        message: "Event Created"
+      })
+    )
     .catch(err => dispatch({ type: ERROR_EVENT }));
 };
 
@@ -29,7 +68,13 @@ export const deleteEvent = url => dispatch => {
   dispatch({ type: DELETING_EVENT });
   axiosWithAuth()
     .delete(url)
-    .then(res => dispatch({ type: SUCCESS_EVENT, payload: res.data, message: "Event Deleted" }))
+    .then(res =>
+      dispatch({
+        type: SUCCESS_EVENT,
+        payload: res.data,
+        message: "Event Deleted"
+      })
+    )
     .catch(err => dispatch({ type: ERROR_EVENT }));
 };
 
