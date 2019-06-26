d7024a8cb27f2db563bb751e974e61ddbb8c5b47
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-06-26 08:08:55 +0100

    Fix: Refactor dashboard to save evetnts retrieved in its personal state

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -3,10 +3,76 @@ import AddEvent from '../components/AddEvent';
 import Event from '../components/Event';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { getAllEvents } from '../store/actions/event';
+import { getAllEvents,createEvent } from '../store/actions/event';
 import { connect } from 'react-redux';
 import {getUserId}from '../utils/jwtDecode';
 import DOMAIN from '../utils/path';
+
+
+class Dashboard extends Component {
+    state = {
+      events: []
+    }
+
+    async componentDidMount() {
+      const user = getUserId();
+      const userId = user.subject;
+
+      const url = `${DOMAIN}/api/events/?user_id=${userId}`;
+      this.props.getAllEvents(url)
+        .then(() => {
+          this.setState({events: this.props.events})
+        })  
+    }
+
+
+    handleSubmit = (data) => {
+      const url = `${DOMAIN}/api/events`;
+      this.props.createEvent(url, data).then(res => {
+        window.location.reload()
+        this.props.history.push('/dashboard')
+      });
+    }
+
+    render() {
+        return (
+          <MainDiv>
+            <AddEvent handleSubmit={this.handleSubmit} />
+            <EventsMainDiv>
+              {this.props.fetchingEvents ? (
+                <div>
+                  <h2>Loading...</h2>
+                </div>
+              ) : this.state.events.length === 0 ? (
+                <div>
+                  <h2>No event found</h2>
+                </div>
+              ) : (
+                ""
+              )}
+
+              {this.state.events.map(event => {
+                return (
+                  <Link key={event.id} to={`/events/${event.id}`}>
+                    <Event title={event.event_title} date={event.event_date} time={event.event_time} completed={event.completed} />
+                  </Link>
+                );
+              })}
+            </EventsMainDiv>
+          </MainDiv>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    events: state.events.events,
+    fetchingEvents: state.events.fetchingEvents
+  }
+}
+
+export default connect(mapStateToProps, {getAllEvents, createEvent})(Dashboard)
+
 
 const MainDiv = styled.div`
   justify-content: space-between;
@@ -30,49 +96,3 @@ const EventsMainDiv = styled.div`
     text-decoration: none;
   }
 `;
-
-class Dashboard extends Component {
-
-    componentDidMount() {
-      const user = getUserId();
-      const userId = user.subject;
-
-      const url = `${DOMAIN}/api/events/?user_id=1`;
-      this.props.getAllEvents(url);
-    }
-
-    render() {
-        return (
-          <MainDiv>
-            <AddEvent />
-            <EventsMainDiv>
-              {this.props.events.length === 0 ? (
-                <div>
-                  <h2>No event found</h2>
-                </div>
-              ) : (
-                ""
-              )}
-              {this.props.events.map(event => {
-                return (
-                  <Link
-                    key={event.id}
-                    to={`/events/${event.id}`}
-                  >
-                    <Event />
-                  </Link>
-                );
-              })}
-            </EventsMainDiv>
-          </MainDiv>
-        );
-    }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    events: state.events.events
-  }
-}
-
-export default connect(mapStateToProps, {getAllEvents})(Dashboard)
